0ea1d4da7a3935c0efb6ccaaed16c64a7ca0e194
Tianwen Chen <chentianwen@hotmail.com>
2012-04-15 21:04:56 +1000

    refactoring the coffeescript

--- src/jquery.dropdown_list_chain.js
+++ src/jquery.dropdown_list_chain.js
@@ -11,27 +11,7 @@
  * http://www.opensource.org/licenses/MIT
  *
  *
- * Usage:
- *
- * HTML Markup:
- * <select id='country' name='country'>
- *   <option />
- *   <option value='au'>Australia</option>
- *   <option value='us'>United Status</option>
- * </select>
- * <select id='state' name='state'>
- *   <option value='nsw' data-chain='au'>NSW</option>
- *   <option value='act' data-chain='au'>ACT</option>
- *   <option value='vic' data-chain='au'>VIC</option>
- *   <option value='ny' data-chain='us'>NY</option>
- *   <option value='ny' data-chain='us'>NY</option>
- *   <option value='ny' data-chain='us'>NY</option>
- * </select>
- *
- * javascript:
- *  $element.chain(settings = {})
- * or
- *  $child.chainedTo($parent, settings = {})
+ * @document http://chentianwen.github.com/jquery_dropdown_list_chain/
 */
 
 (function() {
@@ -41,17 +21,18 @@
     var SelectChain;
     $.chain = {
       defaults: {
-        ajax: false,
-        ajax_mapping: {
-          text: 'text',
-          value: 'value',
-          build_option: false
-        },
         include_blank: {
           text: ' - ',
           value: ''
         },
-        keep_last_value: true
+        remember_last_value: true,
+        ajax: false,
+        ajax_mapping: {
+          text: 'text',
+          value: 'value',
+          build_option: false,
+          filter: false
+        }
       },
       version: '0.9',
       name: 'jQuery Dropdown List Chain',
@@ -64,7 +45,7 @@
       return this.each(function() {
         if (typeof chaineeElement === 'string') chaineeElement = $(chaineeElement);
         if (!SelectChain.is_select(chaineeElement)) {
-          chaineeElement = $("select[data-target='#" + this.id + "'][data-toggle=chain]");
+          chaineeElement = $("select[data-toggle=chain][data-target='#" + this.id + "']");
         }
         return $(chaineeElement).chainedTo(this, settings);
       });
@@ -72,7 +53,6 @@
     $.fn.chainedTo = function(chainerElement, settings) {
       if (chainerElement == null) chainerElement = null;
       if (settings == null) settings = {};
-      settings = $.extend({}, $.chain.defaults, settings);
       if (typeof chainerElement === 'string') {
         chainerElement = $(chainerElement)[0];
       }
@@ -83,6 +63,7 @@
           chainerElement = $($(this).data('target'))[0];
         }
         if (!chainerElement) return;
+        settings = $.extend({}, $.chain.defaults, $(this).data(), settings);
         setup = $(this).data($.chain.attribute_name);
         if (!(setup instanceof SelectChain)) {
           setup = new SelectChain($(chainerElement), $(this));
@@ -112,34 +93,8 @@
         this.$chainee = $chainee;
         this.$clone = this.$chainee.clone();
         this.last_selected = {};
-        this.chain_them_together();
+        this._chain_them_together();
       }
-
-      SelectChain.prototype.chain_them_together = function() {
-        if (!this.$chainer.attr('id')) {
-          this.$chainer.attr('id', "chain_" + (parseInt(Math.random() * 10000)) + (new Date().getTime()));
-        }
-        return this.$chainee.attr('data-toggle', 'chain').attr('data-target', "#" + (this.$chainer.attr('id')));
-      };
-
-      SelectChain.prototype.update_last_selected = function() {
-        if (this.settings.keep_last_value) {
-          return this.last_selected[this.$chainer.val()] = this.$chainee.val();
-        }
-      };
-
-      SelectChain.prototype.cleanup = function() {
-        var include_blank;
-        this.$chainee.children().remove();
-        include_blank = this.settings.include_blank;
-        if (include_blank) {
-          return this.$chainee.append(this.build_option(include_blank.text, include_blank.value));
-        }
-      };
-
-      SelectChain.prototype.build_option = function(text, value) {
-        return $('<option />').text(text).attr('value', value);
-      };
 
       SelectChain.prototype.update = function(settings) {
         this.settings = settings;
@@ -148,37 +103,85 @@
         return this;
       };
 
-      SelectChain.prototype.reload = function() {
-        this.cleanup();
-        if (this.settings.ajax) {
-          this.load_remote_options();
-        } else {
-          this.load_local_options();
+      SelectChain.prototype.update_last_selected = function() {
+        if (this.settings.remember_last_value) {
+          return this.last_selected[this.$chainer.val()] = this.$chainee.val();
         }
-        if (this.settings.keep_last_value) {
+      };
+
+      SelectChain.prototype.reload = function() {
+        var ajax_settings;
+        this._cleanup_chainee_options();
+                if ((ajax_settings = this._load_ajax_settings()) != null) {
+          ajax_settings = this._load_ajax_settings();
+        } else {
+          this._load_options_from_remote_with({
+            ajax_settings: this._load_options_from_local()
+          });
+        };
+        return this._load_last_selected();
+      };
+
+      SelectChain.prototype._chain_them_together = function() {
+        if (!this.$chainer.attr('id')) {
+          this.$chainer.attr('id', "chain_" + (parseInt(Math.random() * 10000)) + (new Date().getTime()));
+        }
+        return this.$chainee.attr({
+          'data-toggle': 'chain',
+          'data-target': "#" + (this.$chainer.attr('id'))
+        });
+      };
+
+      SelectChain.prototype._cleanup_chainee_options = function() {
+        var include_blank;
+        this.$chainee.children().remove();
+        if (include_blank = this.settings.include_blank) {
+          return this.$chainee.append(this._build_option(include_blank.text, include_blank.value));
+        }
+      };
+
+      SelectChain.prototype._load_last_selected = function() {
+        if (this.settings.remember_last_value) {
           return this.$chainee.val(this.last_selected[this.$chainer.val()]);
         }
       };
 
-      SelectChain.prototype.load_remote_options = function() {
-        var mapping, self;
-        self = this;
-        mapping = this.settings.ajax_mapping;
-        return $.ajax(this.settings.ajax).success(function(data, textStatus, jqXHR) {
-          return $.each(data, function(index, record) {
-            if (mapping.build_option) {
-              return self.$element.append(mapping.build_option(record));
-            } else {
-              return self.$element.append(self.build_option(record[mapping.text], record[mapping.value]));
-            }
-          });
-        });
+      SelectChain.prototype._build_option = function(text, value) {
+        return $('<option />').text(text).attr('value', value);
       };
 
-      SelectChain.prototype.load_local_options = function() {
+      SelectChain.prototype._load_options_from_local = function() {
         if (this.$clone) {
           return this.$chainee.append(this.$clone.find("option[data-chain='" + (this.$chainer.val()) + "']").clone());
         }
+      };
+
+      SelectChain.prototype._load_ajax_settings = function() {
+        var ajax, _ref;
+        if (ajax = this.settings.ajax) {
+          return (_ref = !$.isFunction(ajax)) != null ? _ref : {
+            ajax: ajax()
+          };
+        }
+      };
+
+      SelectChain.prototype._load_options_from_remote_with = function(ajax_settings) {
+        var process;
+        process = $.proxy(this._map_each_record, this);
+        return $.ajax(ajax_settings).success(function(data, textStatus, jqXHR) {
+          return $.each(data, process);
+        });
+      };
+
+      SelectChain.prototype._map_each_record = function(index, record) {
+        var build_option, filter, mapping;
+        if ((filter = this.settings.ajax_mapping.filter) && filter(record, this.$chainer.val())) {
+          return;
+        }
+        if ((build_option = this.settings.ajax_mapping.build_option) && build_option(record)) {
+          return;
+        }
+        return (mapping = this.settings.ajax_mapping) && this.$chainee.append(this._build_option(record[mapping.text], record[mapping.value]));
       };
 
       return SelectChain;
--- src/jquery.dropdown_list_chain.js.coffee
+++ src/jquery.dropdown_list_chain.js.coffee
@@ -10,27 +10,7 @@
  * http://www.opensource.org/licenses/MIT
  *
  *
- * Usage:
- *
- * HTML Markup:
- * <select id='country' name='country'>
- *   <option />
- *   <option value='au'>Australia</option>
- *   <option value='us'>United Status</option>
- * </select>
- * <select id='state' name='state'>
- *   <option value='nsw' data-chain='au'>NSW</option>
- *   <option value='act' data-chain='au'>ACT</option>
- *   <option value='vic' data-chain='au'>VIC</option>
- *   <option value='ny' data-chain='us'>NY</option>
- *   <option value='ny' data-chain='us'>NY</option>
- *   <option value='ny' data-chain='us'>NY</option>
- * </select>
- *
- * javascript:
- *  $element.chain(settings = {})
- * or
- *  $child.chainedTo($parent, settings = {})
+ * @document http://chentianwen.github.com/jquery_dropdown_list_chain/
 ###
 jQuery ($) ->
   "use_strict"
@@ -38,15 +18,16 @@ jQuery ($) ->
   # options
   $.chain =
     defaults:
-      ajax: false
-      ajax_mapping:
-        text: 'text'
-        value: 'value'
-        build_option: false # function(record) { return $('<option />').text(record.text).attr('value', record.value) }
       include_blank:
         text: ' - '
         value: ''
-      keep_last_value: true
+      remember_last_value: true
+      ajax: false # function(current_chainer_value) { return ajax_option; }
+      ajax_mapping:
+        text: 'text'
+        value: 'value'
+        build_option: false # function(record) { return $('<option />'); }
+        filter: false # function(record, chainer_value) { return false; }
     version: '0.9'
     name: 'jQuery Dropdown List Chain'
     attribute_name: 'jquery-dropdown-list-chain-setup'
@@ -55,17 +36,18 @@ jQuery ($) ->
   $.fn.chain = (chaineeElement = null, settings = {}) ->
     this.each ->
       chaineeElement = $(chaineeElement) if typeof chaineeElement is 'string' # css selector?
-      chaineeElement = $("select[data-target='##{ this.id }'][data-toggle=chain]") unless SelectChain.is_select chaineeElement
+      chaineeElement = $("select[data-toggle=chain][data-target='##{ this.id }']") unless SelectChain.is_select chaineeElement
       $(chaineeElement).chainedTo this, settings
 
   $.fn.chainedTo = (chainerElement = null, settings = {}) ->
-    settings = $.extend {}, $.chain.defaults, settings
     chainerElement = $(chainerElement)[0] if typeof chainerElement is 'string' # css selector?
     this.each ->
       return unless SelectChain.is_select this
       # default chainerElement
       chainerElement = $($(this).data 'target')[0] unless SelectChain.get_element chainerElement
       return unless chainerElement
+
+      settings = $.extend {}, $.chain.defaults, $(this).data(), settings
 
       # update setup
       setup = $(this).data $.chain.attribute_name
@@ -85,49 +67,58 @@ jQuery ($) ->
       return false unless obj = SelectChain.get_element(obj)
       /select/i.test obj['tagName']
 
+    # public
     constructor: (@$chainer, @$chainee) ->
       @$clone         = @$chainee.clone() # keep a copy
       @last_selected  = {}
-      @chain_them_together()
-
-    chain_them_together: -> # force to establish the connection between them
-      @$chainer.attr 'id', "chain_#{ parseInt Math.random() * 10000 }#{ new Date().getTime() }" unless @$chainer.attr 'id'
-      @$chainee.attr('data-toggle', 'chain').attr 'data-target', "##{ @$chainer.attr 'id' }"
-
-    update_last_selected: -> # on chainee: to save current value
-      @last_selected[ @$chainer.val() ] = @$chainee.val() if @settings.keep_last_value
-
-    cleanup: ->
-      @$chainee.children().remove()
-      include_blank = @settings.include_blank
-      @$chainee.append @build_option(include_blank.text, include_blank.value) if include_blank
-
-    build_option: (text, value) ->
-      $('<option />').text(text).attr 'value', value
+      @_chain_them_together()
 
     update: (@settings) ->
-      @update_last_selected(); @reload(); @
+      @update_last_selected()
+      @reload()
+      @
+
+    update_last_selected: -> # on chainee: to save current value
+      @last_selected[ @$chainer.val() ] = @$chainee.val() if @settings.remember_last_value
 
     reload: -> # on chainer: to reload chainee options
-      @cleanup()
-      if @settings.ajax
-        @load_remote_options()
-      else
-        @load_local_options()
-      @$chainee.val(@last_selected[ @$chainer.val() ]) if @settings.keep_last_value
+      @_cleanup_chainee_options()
+      (ajax_settings = @_load_ajax_settings()) ? @_load_options_from_remote_with ajax_settings : @_load_options_from_local()
+      @_load_last_selected()
 
-    load_remote_options: ->
-      self    = @
-      mapping = @settings.ajax_mapping
-      $.ajax(@settings.ajax).success (data, textStatus, jqXHR) ->
-        $.each data, (index, record) ->
-          if mapping.build_option
-            self.$element.append mapping.build_option(record)
-          else
-            self.$element.append self.build_option(record[mapping.text], record[mapping.value])
+    # private
+    _chain_them_together: -> # by force
+      @$chainer.attr 'id', "chain_#{ parseInt Math.random() * 10000 }#{ new Date().getTime() }" unless @$chainer.attr 'id'
+      @$chainee.attr
+        'data-toggle': 'chain'
+        'data-target': "##{ @$chainer.attr 'id' }"
 
-    load_local_options: ->
+    _cleanup_chainee_options: ->
+      @$chainee.children().remove()
+      @$chainee.append @_build_option(include_blank.text, include_blank.value) if include_blank = @settings.include_blank
+
+    _load_last_selected: ->
+      @$chainee.val(@last_selected[ @$chainer.val() ]) if @settings.remember_last_value
+
+    _build_option: (text, value) ->
+      $('<option />').text(text).attr 'value', value
+
+    _load_options_from_local: ->
       @$chainee.append @$clone.find("option[data-chain='#{ @$chainer.val() }']").clone() if @$clone
+
+    # ajax
+    _load_ajax_settings: ->
+      ! $.isFunction(ajax) ? ajax : ajax() if ajax = @settings.ajax
+
+    _load_options_from_remote_with: (ajax_settings) ->
+      process = $.proxy @_map_each_record, @
+      $.ajax(ajax_settings).success (data, textStatus, jqXHR) ->
+        $.each data, process
+
+    _map_each_record: (index, record) ->
+      return if (filter = @settings.ajax_mapping.filter) && filter(record, @$chainer.val())
+      return if (build_option = @settings.ajax_mapping.build_option) && build_option(record)
+      (mapping = @settings.ajax_mapping) and @$chainee.append @_build_option(record[mapping.text], record[mapping.value])
 
   SelectChain = $.SelectChain
 
